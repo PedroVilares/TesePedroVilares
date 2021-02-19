3d41b36a4dfdfeb602960c95a316b003c8903594
PedroVilares <47793686+PedroVilares@users.noreply.github.com>
2021-02-19 14:27:11 +0000

    Add files via upload

--- models.py
+++ models.py
@@ -1,28 +1,34 @@
 import tensorflow as tf
 from tensorflow import keras
-from keras import metrics,callbacks,layers
+from keras import metrics,optimizers,losses
 from keras.callbacks import Callback
 from keras.models import Sequential
-from keras.layers import Dense, Conv2D, MaxPooling2D,Dropout,GlobalAveragePooling2D,Input, concatenate
+from keras.layers import Flatten,Add,BatchNormalization,Dense,Conv2D,MaxPooling2D,Dropout,GlobalAveragePooling2D,Input,concatenate
 
-def train_model(model,optimizer,loss_function,metrics,epochs,steps_per_epoch,training_data,callbacks):
+def make_predict(test_data):
     """
-    Fits the model to target data using said hyperparameters
+    Predicts target data using a previously trained model
     """
 
-    model.compile(
-        optimizer= str(optimizer),
-        loss= loss_function,
-        metrics= metrics)
+    loaded_model = keras.models.load_model("D:/BCDR/vgg19_trained",compile=True)
     
     with tf.device('/gpu'):
-        hist = model.fit(training_data,
-        epochs= epochs,
-        steps_per_epoch=steps_per_epoch,
-        callbacks = callbacks
-        )
+        predictions = loaded_model.predict(test_data)
     
-    return hist
+    y_pred=[]
+    for i in range(len(predictions)):
+        y_pred.append(predictions[i][0])
+    
+    return y_pred
+
+def load_model():
+    try:
+        loaded_model = keras.models.load_model("D:/BCDR/vgg19_trained",compile=True)
+        print("Successfully loaded DL model!")
+        return loaded_model
+    except OSError:
+        print("Failed to load DL model!") 
+        return 
 
 def create_sequential_model(size1,size2):
 
@@ -66,73 +72,6 @@ def create_sequential_model(size1,size2):
 
     #Output Layer
     model.add(Dense(2,activation='softmax'))
-
-    return model
-
-def create_functional_model(size1,size2):
-    """
-    Create multiple input model
-    """
-#Craniocaudal Block
-    #Input
-    cc_image_input = keras.Input(shape=(size1,size2,1),name='CC_Input')
-    cc_image_1 = keras.layers.Conv2D(32,kernel_size=(3, 3),strides=(2,2),activation='relu')(cc_image_input)
-    cc_image_2 = keras.layers.MaxPooling2D(pool_size=(3,3),strides=(3,3))(cc_image_1)
-    #First Block
-    cc_image_3 = keras.layers.Conv2D(64,kernel_size=(3, 3),strides=(2, 2),activation='relu')(cc_image_2)
-    cc_image_4 = keras.layers.Conv2D(64,kernel_size=(3, 3),strides=(1, 1),activation='relu')(cc_image_3)
-    cc_image_5 = keras.layers.Conv2D(64,kernel_size=(3, 3),strides=(1, 1),activation='relu')(cc_image_4)
-    cc_image_6 = keras.layers.MaxPooling2D(pool_size=(2,2),strides=(2, 2))(cc_image_5)
-    #Second Block
-    cc_image_7 = keras.layers.Conv2D(128,kernel_size=(3, 3),strides=(1, 1),activation='relu')(cc_image_6)
-    cc_image_8 = keras.layers.Conv2D(128,kernel_size=(3, 3),strides=(1, 1),activation='relu')(cc_image_7)
-    cc_image_9 = keras.layers.Conv2D(128,kernel_size=(3, 3),strides=(1, 1),activation='relu')(cc_image_8)
-    cc_image_10 = keras.layers.MaxPooling2D(pool_size=(2,2),strides=(2, 2))(cc_image_9)
-    #Third Block
-    cc_image_11 = keras.layers.Conv2D(128,kernel_size=(3, 3),strides=(1, 1),activation='relu')(cc_image_10)
-    cc_image_12 = keras.layers.Conv2D(128,kernel_size=(3, 3),strides=(1, 1),activation='relu')(cc_image_11)
-    cc_image_13 = keras.layers.Conv2D(128,kernel_size=(3, 3),strides=(1, 1),activation='relu')(cc_image_12)
-    cc_image_14 = keras.layers.MaxPooling2D(pool_size=(2,2),strides=(2, 2))(cc_image_13)
-    #Fourth Block   
-    cc_image_15 = keras.layers.Conv2D(256,kernel_size=(3, 3),strides=(1, 1),activation='relu')(cc_image_14)
-    cc_image_16 = keras.layers.Conv2D(256,kernel_size=(3, 3),strides=(1, 1),activation='relu')(cc_image_15)
-    cc_image_17 = keras.layers.Conv2D(256,kernel_size=(3, 3),strides=(1, 1),activation='relu')(cc_image_16)
-    cc_image_18 = keras.layers.GlobalAveragePooling2D()(cc_image_17)
-
-#Mediolateral Oblique Block
-    #Input
-    mlo_image_input = keras.Input(shape=(size1,size2,1),name='MLO_Input')
-    mlo_image_1 = keras.layers.Conv2D(32,kernel_size=(3, 3),strides=(2,2),activation='relu')(mlo_image_input)
-    mlo_image_2 = keras.layers.MaxPooling2D(pool_size=(3,3),strides=(3,3))(mlo_image_1)
-    #First Block
-    mlo_image_3 = keras.layers.Conv2D(64,kernel_size=(3, 3),strides=(2, 2),activation='relu')(mlo_image_2)
-    mlo_image_4 = keras.layers.Conv2D(64,kernel_size=(3, 3),strides=(1, 1),activation='relu')(mlo_image_3)
-    mlo_image_5 = keras.layers.Conv2D(64,kernel_size=(3, 3),strides=(1, 1),activation='relu')(mlo_image_4)
-    mlo_image_6 = keras.layers.MaxPooling2D(pool_size=(2,2),strides=(2, 2))(mlo_image_5)
-    #Second Block
-    mlo_image_7 = keras.layers.Conv2D(128,kernel_size=(3, 3),strides=(1, 1),activation='relu')(mlo_image_6)
-    mlo_image_8 = keras.layers.Conv2D(128,kernel_size=(3, 3),strides=(1, 1),activation='relu')(mlo_image_7)
-    mlo_image_9 = keras.layers.Conv2D(128,kernel_size=(3, 3),strides=(1, 1),activation='relu')(mlo_image_8)
-    mlo_image_10 = keras.layers.MaxPooling2D(pool_size=(2,2),strides=(2, 2))(mlo_image_9)
-    #Third Block
-    mlo_image_11 = keras.layers.Conv2D(128,kernel_size=(3, 3),strides=(1, 1),activation='relu')(mlo_image_10)
-    mlo_image_12 = keras.layers.Conv2D(128,kernel_size=(3, 3),strides=(1, 1),activation='relu')(mlo_image_11)
-    mlo_image_13 = keras.layers.Conv2D(128,kernel_size=(3, 3),strides=(1, 1),activation='relu')(mlo_image_12)
-    mlo_image_14 = keras.layers.MaxPooling2D(pool_size=(2,2),strides=(2, 2))(mlo_image_13)
-    #Fourth Block 
-    mlo_image_15 = keras.layers.Conv2D(256,kernel_size=(3, 3),strides=(1, 1),activation='relu')(mlo_image_14)
-    mlo_image_16 = keras.layers.Conv2D(256,kernel_size=(3, 3),strides=(1, 1),activation='relu')(mlo_image_15)
-    mlo_image_17 = keras.layers.Conv2D(256,kernel_size=(3, 3),strides=(1, 1),activation='relu')(mlo_image_16)
-    mlo_image_18 = keras.layers.GlobalAveragePooling2D()(mlo_image_17)
-
-#Concatenation
-    image_1 = keras.layers.concatenate([cc_image_18,mlo_image_18])
-    image_2 = keras.layers.Dense(1024,activation='relu')(image_1)
-    cc_output = keras.layers.Dense(2,activation='softmax')(image_2)
-    mlo_output = keras.layers.Dense(2,activation='softmax')(image_2)
-
-#Building the Model
-    model = keras.Model(inputs=[cc_image_input,mlo_image_input],outputs=[cc_output,mlo_output],name = 'functional_model')
 
     return model
 
